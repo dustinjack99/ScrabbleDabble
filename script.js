71d0d28d2b157697101f60c1efa9b7a28ddf6f39
Jon Lee <jl830630@gmail.com>
2020-01-23 19:19:18 -0800

    put up score and tiles left

--- script.js
+++ script.js
@@ -5,15 +5,23 @@ let tileNum = document.querySelector("#tile-num");
 let input = document.querySelector("#input");
 let submit = document.querySelector("#submit");
 let letterValue = document.querySelectorAll(".letter-value");
-let letterFace = document.querySelectorAll(".letter")
-let form = document.querySelector(".pure-form")
+let letterFace = document.querySelectorAll(".letter");
+let form = document.querySelector(".pure-form");
+let score = document.querySelector("#score");
+let userScore = 0
+score.textContent = "score : " + userScore;
+
 
 
 let scores = { 'A' : 1, 'B' : 3, 'C' : 3, 'D' : 2, 'E' : 1, 'F' : 4, 'G' : 2, 'H' : 4, 'I' : 1, 'J' : 8, 'K' : 5, 'L' : 1, 'M' : 3, 'N' : 1, 'O' : 1, 'P' : 3, 'Q' : 10, 'R' : 1, 'S' : 1, 'T' : 1, 'U' : 1, 'V' : 4, 'W' : 4, 'X' : 8, 'Y' : 4, 'Z' : 10 };
 let spl = "AAAAAAAAABBCCDDDDEEEEEEEEEEEEFFGGGHHIIIIIIIIIJKLLLLMMNNNNNNOOOOOOOOPPQRRRRRRSSSSTTTTTTUUUUVVWWXYYZ";
 
 let tileBag = spl.split("");
-// console.log(spl);
+console.log(tileBag);
+let leftTiles = tileBag.length;
+console.log(leftTiles);
+tileNum.textContent = "tiles left : " + leftTiles;
+
 
 let sum = 0;
 
