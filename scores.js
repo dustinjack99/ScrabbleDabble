8b70ae7c0964b21ac27948a0121ad842807d9842
Austen Turner <austenpturner@msn.com>
2020-01-28 18:15:04 -0800

    add responsive design for highscores page and delete icons

--- scores.js
+++ scores.js
@@ -1,13 +1,32 @@
+let scoreDiv;
+let li;
+let deleteBtn;
 let list = JSON.parse(localStorage.getItem("highscores"));
 let scoreBoard = document.querySelector("#scoreboard");
+
 console.log(list);
+
 list.sort(function (a, b) {
   return b.score - a.score;
 });
 
 for (i = 0; i < list.length; i++) {
-  let li = document.createElement("li");
-  scoreBoard.appendChild(li);
+  scoreDiv = document.createElement("div");
+  scoreBoard.appendChild(scoreDiv);
+  li = document.createElement("li");
   li.textContent = list[i].user + "'s score:   " + list[i].score;
+  scoreDiv.appendChild(li);
+  deleteBtn = document.createElement("i");
+  deleteBtn.className = 'fas fa-times';
+  scoreDiv.appendChild(deleteBtn);
 }
 
+// function to delete score from local storage
+function deleteScore() {
+  JSON.parse(localStorage.getItem("highscores"));
+}
+
+// event listener on delete icon to call delete function
+scoreDiv.addEventListener('click', function() {
+  deleteScore();
+})
