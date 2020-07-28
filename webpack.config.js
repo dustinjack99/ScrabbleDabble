c47511177f5a31f173c2f3d442f4b3b6932ad2ac
dustinjack99 <57464275+dustinjack99@users.noreply.github.com>
2020-07-28 14:17:52 -0700

    got everything to compile to dist, and vanilla js. now to upload to azure storage and see if it worked

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,26 @@
+const path = require('path');
+
 module.exports = {
-  entry: 'landing.html',
+  //   context: __dirname,
+  entry: './src/scores.ts',
   output: {
-    path: __dirname + '/dist',
-    filename: '',
+    filename: 'scores.js',
+    path: path.resolve(__dirname, 'dist'),
+    // publicPath: '/dist/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: {
+          loader: 'ts-loader',
+        },
+        exclude: /node_modules/,
+        include: [path.resolve(__dirname, 'src')],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts'],
   },
 };
