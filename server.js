b48b3eb7ca2e3023139c17d838ea9e1751b89923
dustinjack99 <dustin.guy.jackson@gmail.com>
2021-10-13 09:34:26 -0700

    added server file, fixed names of html files, all app being served on appropriate deployment port

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, "/dist")));
+
+app.listen(port, () => {
+  console.log(`App is listening on port: ${port}`);
+});
